0835d4ec7aab5f1595194fcd48835419025367a1
Your Name <you@example.com>
2025-06-10 03:38:11 -0500

    Inicializando proyecto

---
+++ src/main/java/com/utp/SanSilvestreDePaulBackApplication.java
@@ -0,0 +1,13 @@
+package com.utp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SanSilvestreDePaulBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SanSilvestreDePaulBackApplication.class, args);
+	}
+
+}
